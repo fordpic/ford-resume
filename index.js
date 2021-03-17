996bfa46165dd6ca84e38516cedf68b4cf88f6e8
fordpic <fordpickert@gmail.com>
2021-03-17 14:09:23 -0500

    added colors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
+const colors = require('colors');
 
 fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
 	if (err) {
 		console.log(err);
 	} else {
 		console.log(data);
-		return data;
+		return data.colors.rainbow;
 	}
 });
